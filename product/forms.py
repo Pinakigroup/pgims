3d24fe580b869e386d7138d4468b92e04c532afb
pinakigroup <pinakigroup.sd@gmail.com>
2022-11-16 15:47:23 +0600

    Add Product Model of the Project

--- product/forms.py
+++ product/forms.py
@@ -4,11 +4,11 @@ from django.forms import fields, widgets
 from .models import Product
 
 class ProductForm(forms.ModelForm):
-    name = forms.CharField(label='Product Name', widget=forms.TextInput(attrs={'class': 'span6 typeahead'}), required=True, error_messages={'required':'Must Enter a Correct Name'})
-    description = forms.CharField(label="Description", widget=forms.Textarea({'class': 'span6 typeahead', 'placeholder':'Text ...', 'rows':8, 'cols':256}), required=True, error_messages={'required':'Must Enter Descriptions'})
+    name = forms.CharField(label='Product Name', widget=forms.TextInput(attrs={'class': 'form-control'}), required=True, error_messages={'required':'Must Enter a Correct Name'})
+    description = forms.CharField(label="Description", widget=forms.Textarea({'class': 'form-control', 'placeholder':'Text ...', 'rows':8, 'cols':256}), required=True, error_messages={'required':'Must Enter Descriptions'})
     class Meta:
         model = Product
         fields = ['id', 'name', 'category', 'description']
         widgets = {
-            'category': forms.Select(attrs={'class': 'span6 typeahead'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
         }

644d94593403ad6979b90b2e5be8f3577b130978
pinakigroup <pinakigroup.sd@gmail.com>
2022-11-19 17:03:34 +0600

    add registerform in this project

--- pgims/forms.py
+++ pgims/forms.py
@@ -4,6 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
+    # email = forms.EmailField(required=True)
     class Meta:
         model = User
-        fields = ('email', 'username', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+	# def save(self, commit=True):
+	# 	user = super(NewUserForm, self).save(commit=False)
+	# 	user.email = self.cleaned_data['email']
+	# 	if commit:
+	# 		user.save()
+	# 	return user        

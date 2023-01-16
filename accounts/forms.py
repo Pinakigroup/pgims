a1b35c835caa70bcae505bce12dcdbec93a16045
pinakigroup <pinakigroup.sd@gmail.com>
2023-01-16 16:57:39 +0600

    get date from Usercreationform not working

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,14 +5,21 @@ from django import forms
 
 class RegisterForm(UserCreationForm):
     # email = forms.EmailField(required=True)
+    birthdate = forms.DateField()
+    discord_id = forms.CharField(max_length=100, help_text='Discord ID')
+    zoom_id = forms.CharField(max_length=100, help_text='Zoom ID')
+    
     designation = forms.CharField(max_length=64, required=True)
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'designation', 'password1', 'password2']  
+        fields = ['first_name', 'last_name', 'username', 'email', 'designation', 'birthdate', 'discord_id', 'zoom_id', 'password1', 'password2'] 
         
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
         user.designation = self.cleaned_data["designation"]
+        user.birthdate = self.cleaned_data["birthdate"]
+        user.discord_id = self.cleaned_data["discord_id"]
+        user.zoom_id = self.cleaned_data["zoom_id"]
         if commit:
             user.save()
         return user   

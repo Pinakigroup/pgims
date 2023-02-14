24e6f7f1615bb53de78cec882cecf3d7defc3ce5
pinakigroup <pinakigroup.sd@gmail.com>
2023-02-14 16:26:32 +0600

    makemigrations and migrate command add

--- pgims/settings.py
+++ pgims/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_URL = 'login'
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

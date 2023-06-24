63e979a11b2805719221475fa585d6fe99fea802
afjaluddin <afjaluddin@gmail.com>
2023-06-24 17:01:24 +0600

    blank commit

--- pgims/settings.py
+++ pgims/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     # Other apps...
     'django_select2',
     'rest_framework',

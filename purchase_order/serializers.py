eeace76cbb22943bc695012358d9bd918a643e76
Plabon <plabondatta26@gmail.com>
2023-11-16 23:10:28 +0600

    purchase item serializer updated with foreignkey details

--- purchase_order/serializers.py
+++ purchase_order/serializers.py
@@ -12,6 +12,25 @@ class PurchaseBillSerializer(serializers.ModelSerializer):
         
 
 class PurchaseItemSerializer(serializers.ModelSerializer):
+    stock = serializers.SerializerMethodField()
+    unit = serializers.SerializerMethodField()
+    received_quantity = serializers.SerializerMethodField()
+
+    def get_stock(self, instance):
+        return {'id': instance.stock.id if instance.stock else '', 'name': instance.stock.name if instance.stock else ''}
+
+    def get_unit(self, instance):
+        return {'id': instance.unit.id if instance.id else '', 'unit': instance.unit.unit if instance.unit else ''}
+
+    def get_received_quantity(self, instance):
+        work_order = instance.billno.work_order if instance.billno else None
+        if work_order:
+            obj = StoreItem.objects.filter(billno__work_order__work_order=work_order,
+                                           size=instance.size, style=instance.style, color=instance.color).first()
+            if obj:
+                return obj.received_quantity
+        return 0
+
     class Meta:
         model = PurchaseItem
         # fields = ['quantity', 'size', 'style', 'color']
